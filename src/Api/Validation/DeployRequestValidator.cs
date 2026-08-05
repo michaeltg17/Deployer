34351d9494133fe474c332167d7f563145da04cb
Michael Trullas Garcia <adoser2@hotmail.es>
2026-08-06 00:42:16 +0200

    fix int tests

--- src/Api/Validation/DeployRequestValidator.cs
+++ src/Api/Validation/DeployRequestValidator.cs
@@ -7,10 +7,10 @@ public sealed class DeployRequestValidator : AbstractValidator<DeployRequest>
 {
     public DeployRequestValidator(IDeployerSettings settings)
     {
+        RuleFor(x => x.Project).NotEmpty();
         RuleFor(x => x.Project)
-            .NotEmpty()
             .Must((request, project) => ComposeFileExists(project, settings))
-                .WithMessage((request, project) => $"Docker compose file not found for project '{project}': docker-compose.yml");
+            .WithMessage((request, project) => $"Docker compose file not found for project '{project}': docker-compose.yml");
 
         RuleFor(x => x.Environment).NotEmpty();
         RuleFor(x => x.Tag).NotEmpty();
@@ -18,7 +18,8 @@ public sealed class DeployRequestValidator : AbstractValidator<DeployRequest>
 
     static bool ComposeFileExists(string? project, IDeployerSettings settings)
     {
-        var projectDir = Path.Combine(settings.ProjectsDir, project!);
+        if (string.IsNullOrEmpty(project)) return true;
+        var projectDir = Path.Combine(settings.ProjectsDir, project);
         return File.Exists(Path.Combine(projectDir, "docker-compose.yml"));
     }
 }
